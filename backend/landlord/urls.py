517a0941feb16c595d4bf51a42cbe9fe08f11f90
angelinarianti01 <angelinarianti23@gmail.com>
2023-02-22 10:23:21 +1100

    fix naming bugs

---
+++ backend/landlord/urls.py
@@ -0,0 +1,22 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from . import views
+
+# CRUD API
+router = routers.DefaultRouter()
+
+router.register(r'users', views.UserList)
+router.register(r'groups', views.GroupList)
+router.register(r'properties', views.PropertiesList)
+
+urlpatterns = [
+    # App API
+    path('profile/', views.ProfileView.as_view()),
+    path('my-properties/', views.PropertiesListRestricted.as_view()),
+    path('my-properties/search/', views.search),
+    path('my-properties/<slug:property_slug>', views.PropertyDetails.as_view()),
+    path('tenants/', views.TenantProfilesList.as_view()),
+]
+
+urlpatterns += router.urls
