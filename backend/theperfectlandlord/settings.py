520860325ef6723e3038e6dd3699adf77051f29b
jamieg <james.gardiner@digitall.net.au>
2023-02-22 20:53:20 +1100

    try to fix login CSRF token try 1

--- backend/theperfectlandlord/settings.py
+++ backend/theperfectlandlord/settings.py
@@ -133,6 +133,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
