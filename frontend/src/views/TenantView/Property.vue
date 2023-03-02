f63e34f86478d797d1dfd3a8aa3d776deae70a97
angelinarianti01 <angelinarianti23@gmail.com>
2023-03-02 14:47:31 +1100

    Connect frontend to backend for tenant properties

--- frontend/src/views/TenantView/Property.vue
+++ frontend/src/views/TenantView/Property.vue
@@ -2,42 +2,38 @@
     <TenantSubNav/>
     <div class="page-property">
         <div class="address-heading">
-            <h1 class="address">8/3 Kooyonkoot Road</h1>
+            <h1 class="address">{{property.address}}</h1>
         </div>
 
         <div class="main-section">
             <div clas="section1">
                 <div class="img-section">
                     <figure @click="changeImage">
-                        <img src="@/assets/chris-robert--ryDtcapIas-unsplash.jpg" class="main-image">
-                        <!-- <img v-bind:src="property.get_image" v-show="image" class="main-image">
-                        <img v-bind:src="property.get_thumbnail" v-show="!image" class="thumbnail"> -->
+                        <img v-bind:src="property.get_image" class="main-image">                    
                     </figure>
                 </div>
                 <button class="applyBtn" @click="generateForm">Apply</button>
             </div>
             <div class="section2">
-                <p><strong><i class="fas fa-dollar-sign"></i> Price: </strong>685/week</p>
-                <p> <strong>About Property: </strong>This charming two-story home boasts three bedrooms, 
-                    two and a half bathrooms, and a spacious living room that's perfect for entertaining guests. 
-                    The kitchen features modern appliances and plenty of counter space for preparing delicious meals.</p>
+                <p><strong><i class="fas fa-dollar-sign"></i> Price: </strong>{{property.price}}/week</p>
+                <p> <strong>About Property: </strong>{{ property.description }}</p>
             </div>
         </div>
         <label> Essentials <i class="fas fa-arrow-down"></i></label>
         <div class="advants">
             <div>
               <span>Bedrooms</span>
-              <div><i class="fas fa-th-large"></i><span>3</span></div>
+              <div><i class="fas fa-th-large"></i><span>{{ property.room }}</span></div>
             </div>
             <div>
               <span>Bathrooms </span>
-              <div><i class="fas fa-shower"></i><span>2</span></div>
+              <div><i class="fas fa-shower"></i><span>{{ property.bathroom }}</span></div>
             </div>
             <div>
               <span>Type </span>
               <div>
                 <i class="fas fa-vector-square"></i
-                ><span>House</span>
+                ><span>{{property.type}}</span>
               </div>
             </div>
           </div>
@@ -45,22 +41,103 @@
           <div class="advants features">
               <div>
                 <span>AirCon</span>
-                <div><i class="fas fa-snowflake"></i><span>Yes</span></div>
+                <div><i class="fas fa-snowflake"></i>
+                    <span v-if="property.airConditioning == 1">Yes</span>
+                    <span v-else-if="property.airConditioning == 0">No</span>
+                </div>
               </div>
               <div>
                 <span>Pool </span>
-                <div><i class="fas fa-swimming"></i><span>No</span></div>
+                <div><i class="fas fa-swimming"></i>
+                    <span v-if="property.pool == 1">Yes</span>
+                    <span v-else-if="property.pool == 0">No</span>
+                </div>
               </div>
               <div>
                 <span>Gym </span>
-                <div>
-                  <i class="fas fa-dumbbell"></i
-                  ><span>Yes</span>
+                <div><i class="fas fa-dumbbell"></i>
+                    <span v-if="property.gym == 1">Yes</span>
+                    <span v-else-if="property.gym == 0">No</span>
                 </div>
               </div>
             </div>
     </div>
 </template>
+
+<script>
+import LandingNavbar from '@/components/LandingNavbar.vue'
+import LandlordSubNav from '@/components/Landlord/LandlordSubNav.vue';
+import TenantSubNav from '@/components/Tenant/TenantSubNav.vue';
+import axios from 'axios'
+
+export default {
+    name: "Property",
+    data() {
+        return {
+            property: {},
+            timer: null,
+            currentIndex: 0,
+            image: false,
+        };
+    },
+    mounted() {
+        this.getProperty();
+        if(this.property != null){
+            this.startSlide();
+        }
+    },
+    methods: {
+        changeImage(){
+            this.image = !this.image
+        },
+        startSlide: function() {
+            this.timer = setInterval(this.next, 4000);
+        },
+        next: function() {
+            this.currentIndex += 1;
+        },
+        prev: function() {
+            this.currentIndex -= 1;
+        },
+
+        generateForm(){
+            this.$router.push('/application-form')
+        },
+
+        async getProperty() {
+            this.$store.commit("setIsLoading", true);
+
+            // Set logged in user token as header
+            const config = {'Authorization': `Token ${localStorage.getItem("token")}`} 
+
+            // Retrieve property unique slug 
+            const property_slug = this.$route.params.property_slug;
+
+            // Query property details
+            await axios
+                .get(`v2/property/properties/${property_slug}/`)
+                .then(response => {
+                console.log(response.data);
+                this.property = response.data;
+                document.title = this.property.address + " | The Perfect Landlord";
+            })
+                .catch(error => {
+                console.log(error);
+            });
+
+            this.$store.commit("setIsLoading", false);
+        },
+    },
+
+    computed:{
+        currentImg: function() {
+            return this.images[Math.abs(this.currentIndex) % this.images.length];
+        },
+    },
+    components: { LandingNavbar, LandlordSubNav, TenantSubNav }
+}
+
+</script>
 
 <style scoped>
     .landlordNav{
@@ -128,70 +205,3 @@
 
     }
 </style>
-
-<script>
-import LandingNavbar from '@/components/LandingNavbar.vue'
-import LandlordSubNav from '@/components/Landlord/LandlordSubNav.vue';
-import TenantSubNav from '@/components/Tenant/TenantSubNav.vue';
-import axios from 'axios'
-
-export default {
-    name: "Property",
-    data() {
-        return {
-            property: {},
-            timer: null,
-            currentIndex: 0,
-            image: false,
-        };
-    },
-    mounted() {
-        this.getProperty();
-        if(this.property != null){
-            this.startSlide();
-        }
-    },
-    methods: {
-        changeImage(){
-            this.image = !this.image
-        },
-        startSlide: function() {
-            this.timer = setInterval(this.next, 4000);
-        },
-        next: function() {
-            this.currentIndex += 1;
-        },
-        prev: function() {
-            this.currentIndex -= 1;
-        },
-
-        generateForm(){
-            this.$router.push('/application-form')
-        },
-
-        async getProperty() {
-            this.$store.commit("setIsLoading", true);
-            const property_slug = this.$route.params.property_slug;
-            await axios
-                .get(`/landlord/my-properties/${property_slug}`)
-                .then(response => {
-                console.log(response.data);
-                this.property = response.data;
-                document.title = this.property.address + " | The Perfect Landlord";
-            })
-                .catch(error => {
-                console.log(error);
-            });
-            this.$store.commit("setIsLoading", false);
-        },
-    },
-
-    computed:{
-        currentImg: function() {
-            return this.images[Math.abs(this.currentIndex) % this.images.length];
-        },
-    },
-    components: { LandingNavbar, LandlordSubNav, TenantSubNav }
-}
-
-</script>
